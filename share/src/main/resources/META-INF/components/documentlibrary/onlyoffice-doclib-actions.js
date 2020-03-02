b0026a8c8f53c5da0729376b341351b25b785515
Andrey Panov <gensurkin@yandex.ru>
2020-03-02 16:44:06 +0300

    open created file in a new tab (fixes #28)

---
+++ share/src/main/resources/META-INF/components/documentlibrary/onlyoffice-doclib-actions.js
@@ -0,0 +1,22 @@
+(function () {
+    YAHOO.Bubbling.fire("registerAction",
+    {
+        actionName: "onOnlyofficeCreateDocx",
+        fn: function (obj) { openAndRefresh(obj, "application/vnd.openxmlformats-officedocument.wordprocessingml.document"); }
+    });
+    YAHOO.Bubbling.fire("registerAction",
+    {
+        actionName: "onOnlyofficeCreateXlsx",
+        fn: function (obj) { openAndRefresh(obj, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"); }
+    });
+    YAHOO.Bubbling.fire("registerAction",
+    {
+        actionName: "onOnlyofficeCreatePptx",
+        fn: function (obj) { openAndRefresh(obj, "application/vnd.openxmlformats-officedocument.presentationml.presentation"); }
+    });
+
+    function openAndRefresh(obj, mime) {
+        window.open("onlyoffice-edit?nodeRef=" + obj.nodeRef + "&new=" + mime);
+        setTimeout(function() { YAHOO.Bubbling.fire("metadataRefresh", obj); }, 1000);
+    }
+})();
