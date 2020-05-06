72e984c857b8f16d6708b9989fb4d39a7e3d9295
Andrew <gensurkin@yandex.ru>
2020-05-06 14:34:04 +0300

    fixed auto formatting error

--- repo/src/main/resources/alfresco/templates/webscripts/onlyoffice/onlyoffice-config.get.js
+++ repo/src/main/resources/alfresco/templates/webscripts/onlyoffice/onlyoffice-config.get.js
@@ -1,14 +1,13 @@
-<
-import resource = "classpath:alfresco/templates/webscripts/org/alfresco/repository/admin/admin-common.lib.js" >
+<import resource="classpath:alfresco/templates/webscripts/org/alfresco/repository/admin/admin-common.lib.js">
 
-    /*
-        Copyright (c) Ascensio System SIA 2020. All rights reserved.
-        http://www.onlyoffice.com
-    */
+/*
+    Copyright (c) Ascensio System SIA 2020. All rights reserved.
+    http://www.onlyoffice.com
+*/
 
-    function main() {
-        model.tools = Admin.getConsoleTools("onlyoffice-config");
-        model.metadata = Admin.getServerMetaData();
-    }
+function main() {
+    model.tools = Admin.getConsoleTools("onlyoffice-config");
+    model.metadata = Admin.getServerMetaData();
+}
 
 main();
