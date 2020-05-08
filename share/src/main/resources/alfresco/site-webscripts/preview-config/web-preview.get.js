f0fb0469b5f97c9f4226944ec9720f4898896353
Andrew <gensurkin@yandex.ru>
2020-05-08 14:20:39 +0300

    fixed an issue with document preview on share links

--- share/src/main/resources/alfresco/site-webscripts/preview-config/web-preview.get.js
+++ share/src/main/resources/alfresco/site-webscripts/preview-config/web-preview.get.js
@@ -6,10 +6,10 @@
 if (model.widgets) {
     for (var i = 0; i < model.widgets.length; i++) {
         var widget = model.widgets[i];
-        if (widget.id == "WebPreview") {
+        if (widget.id == "WebPreview" && url.args.nodeRef) {
             pObj = eval('(' + remote.call("/parashift/onlyoffice/prepare?nodeRef=" + url.args.nodeRef + "&preview=true") + ')');
 
-            if (pObj.previewEnabled && pObj.onlyofficeUrl && pObj.mime) {
+            if (pObj && pObj.previewEnabled && pObj.onlyofficeUrl && pObj.mime) {
                 model.onlyofficeUrl = pObj.onlyofficeUrl;
 
                 widget.options.pluginConditions = jsonUtils.toJSONString([{
