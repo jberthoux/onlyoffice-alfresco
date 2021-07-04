4f35ee9a0d47980518859e48aee652dbf5f83991
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2021-07-04 20:07:02 +0300

    improved check permission for displaying convert button

--- share/src/main/java/com/onlyoffice/web/evaluator/IsConvertPermission.java
+++ share/src/main/java/com/onlyoffice/web/evaluator/IsConvertPermission.java
@@ -5,26 +5,46 @@
 
 package com.onlyoffice.web.evaluator;
 
+import com.onlyoffice.web.scripts.OnlyofficeSettingsQuery;
 import org.alfresco.error.AlfrescoRuntimeException;
 import org.alfresco.web.evaluator.BaseEvaluator;
 import org.json.simple.JSONObject;
 
 public class IsConvertPermission extends BaseEvaluator {
+    private OnlyofficeSettingsQuery onlyofficeSettings;
+
+    public void setOnlyofficeSettings(OnlyofficeSettingsQuery onlyofficeSettings) {
+        this.onlyofficeSettings = onlyofficeSettings;
+    }
 
     @Override
     public boolean evaluate(JSONObject jsonObject) {
         try
         {
-            JSONObject parent = (JSONObject)jsonObject.get("parent");
-            if (parent == null)
-            {
-                return false;
-            }
-            else
-            {
-                JSONObject perm = (JSONObject)parent.get("permissions");
-                JSONObject user = (JSONObject)perm.get("user");
-                return (boolean)user.getOrDefault("CreateChildren", false);
+            if (onlyofficeSettings.getConvertOriginal()) {
+                JSONObject node = (JSONObject)jsonObject.get("node");
+                if (node == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    JSONObject perm = (JSONObject)node.get("permissions");
+                    JSONObject user = (JSONObject)perm.get("user");
+                    return (boolean)user.getOrDefault("Write", false);
+                }
+            } else {
+                JSONObject parent = (JSONObject)jsonObject.get("parent");
+                if (parent == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    JSONObject perm = (JSONObject)parent.get("permissions");
+                    JSONObject user = (JSONObject)perm.get("user");
+                    return (boolean)user.getOrDefault("CreateChildren", false);
+                }
             }
         }
         catch (Exception err)
