234e2db738c67b2b501775085c7ca63d0ff4023e
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2021-07-05 12:34:24 +0300

    file conversion with setting convertOriginal

--- repo/src/main/java/com/parashift/onlyoffice/ConvertAction.java
+++ repo/src/main/java/com/parashift/onlyoffice/ConvertAction.java
@@ -5,10 +5,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.alfresco.error.AlfrescoRuntimeException;
 import org.alfresco.model.ContentModel;
 import org.alfresco.repo.action.executer.ActionExecuterAbstractBase;
 import org.alfresco.service.cmr.action.Action;
 import org.alfresco.service.cmr.action.ParameterDefinition;
+import org.alfresco.service.cmr.coci.CheckOutCheckInService;
 import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.ContentReader;
 import org.alfresco.service.cmr.repository.ContentWriter;
@@ -24,7 +26,6 @@ import org.alfresco.service.namespace.QName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.extensions.webscripts.WebScriptException;
 
 /*
     Copyright (c) Ascensio System SIA 2021. All rights reserved.
@@ -53,64 +54,85 @@ public class ConvertAction extends ActionExecuterAbstractBase {
     @Autowired
     Util util;
 
+    @Autowired
+    ConfigManager configManager;
+
+    @Autowired
+    CheckOutCheckInService checkOutCheckInService;
+
     @Override
     protected void executeImpl(Action action, NodeRef actionedUponNodeRef) {
         if (nodeService.exists(actionedUponNodeRef)) {
             if (permissionService.hasPermission(actionedUponNodeRef, PermissionService.READ) == AccessStatus.ALLOWED) {
-
-                ContentReader reader = contentService.getReader(actionedUponNodeRef, ContentModel.PROP_CONTENT);
-                String mime = reader.getMimetype();
-                String targetMimeParam = converterService.GetModernMimetype(mime);
-                if (targetMimeParam == null) {
-                    try {
-                        reader.getContentInputStream().close();
-                    } catch (Exception e) {
-                        logger.error("Error close stream", e);
+                if (!checkOutCheckInService.isCheckedOut(actionedUponNodeRef) &&
+                        !checkOutCheckInService.isWorkingCopy(actionedUponNodeRef)) {
+                    ContentReader reader = contentService.getReader(actionedUponNodeRef, ContentModel.PROP_CONTENT);
+                    String mime = reader.getMimetype();
+                    String targetMimeParam = converterService.GetModernMimetype(mime);
+                    if (targetMimeParam == null) {
+                        try {
+                            reader.getContentInputStream().close();
+                        } catch (Exception e) {
+                            logger.error("Error close stream", e);
+                        }
+                        logger.debug("Files of " + mime + " MIME-type cannot be converted");
+                        return;
                     }
-                    logger.debug("Files of " + mime + " MIME-type cannot be converted");
-                    return;
-                }
 
-                ChildAssociationRef parentAssoc = nodeService.getPrimaryParent(actionedUponNodeRef);
-                if (parentAssoc == null || parentAssoc.getParentRef() == null) {
-                    logger.debug("Couln't find parent folder");
-                    return;
-                }
+                    ChildAssociationRef parentAssoc = nodeService.getPrimaryParent(actionedUponNodeRef);
+                    if (parentAssoc == null || parentAssoc.getParentRef() == null) {
+                        logger.debug("Couldn't find parent folder");
+                        return;
+                    }
 
-                NodeRef parentRef = parentAssoc.getParentRef();
-
-                if (permissionService.hasPermission(parentRef, PermissionService.CREATE_CHILDREN) == AccessStatus.ALLOWED) {
+                    NodeRef nodeFolder = parentAssoc.getParentRef();
                     String nodeName = (String) nodeService.getProperty(actionedUponNodeRef, ContentModel.PROP_NAME);
-                    String baseName = nodeName.substring(0, nodeName.lastIndexOf('.'));
+                    String title = nodeName.substring(0, nodeName.lastIndexOf('.'));
                     String ext = mimetypeService.getExtension(targetMimeParam);
-                    String newName = baseName + "." + ext;
 
-                    NodeRef node = nodeService.getChildByName(parentRef, ContentModel.ASSOC_CONTAINS, newName);
+                    String newName = util.getCorrectName(nodeFolder, title, ext);
 
-                    Integer i = 0;
-                    while (node != null) {
-                        i++;
-                        newName = baseName + " (" + i + ")." + ext;
-                        node = nodeService.getChildByName(parentRef, ContentModel.ASSOC_CONTAINS, newName);
+                    NodeRef writeNode = null;
+                    Boolean deleteNode = false;
+                    Boolean checkoutNode = false;
+
+                    if (configManager.getAsBoolean("convertOriginal", "false")) {
+                        logger.debug("Updating node");
+                        if (permissionService.hasPermission(actionedUponNodeRef, PermissionService.WRITE) == AccessStatus.ALLOWED) {
+                            util.ensureVersioningEnabled(actionedUponNodeRef);
+                            checkoutNode = true;
+                            writeNode = checkOutCheckInService.checkout(actionedUponNodeRef);
+                            nodeService.setProperty(writeNode, ContentModel.PROP_NAME, newName);
+                        } else {
+                            throw new SecurityException("User don't have the permissions to update this node");
+                        }
+                    } else {
+                        logger.debug("Creating new node");
+                        if (permissionService.hasPermission(nodeFolder, PermissionService.CREATE_CHILDREN) == AccessStatus.ALLOWED) {
+                            Map<QName, Serializable> props = new HashMap<QName, Serializable>(1);
+                            props.put(ContentModel.PROP_NAME, newName);
+                            deleteNode = true;
+                            writeNode = this.nodeService.createNode(nodeFolder, ContentModel.ASSOC_CONTAINS,
+                                    QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, newName), ContentModel.TYPE_CONTENT, props)
+                                    .getChildRef();
+
+                            util.ensureVersioningEnabled(writeNode);
+                        } else {
+                            throw new SecurityException("User don't have the permissions to create child node");
+                        }
                     }
 
-                    logger.debug("Converting '" + nodeName + "' -> '" + newName + "'");
-                    logger.debug("Creating new node");
-                    Map<QName, Serializable> props = new HashMap<QName, Serializable>(1);
-                    props.put(ContentModel.PROP_NAME, newName);
-
-                    node = this.nodeService.createNode(parentRef, ContentModel.ASSOC_CONTAINS,
-                            QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, newName), ContentModel.TYPE_CONTENT, props)
-                            .getChildRef();
-
-                    util.ensureVersioningEnabled(node);
-
-                    ContentWriter writer = this.contentService.getWriter(node, ContentModel.PROP_CONTENT, true);
-                    writer.setMimetype(targetMimeParam);
+                    ContentWriter writer = null;
 
                     try {
                         logger.debug("Invoking .transform()");
-                        converterService.transform(reader, writer, new TransformationOptions(actionedUponNodeRef, null, node, null));
+                        writer = this.contentService.getWriter(writeNode, ContentModel.PROP_CONTENT, true);
+                        writer.setMimetype(targetMimeParam);
+                        converterService.transform(reader, writer, new TransformationOptions(actionedUponNodeRef, null, writeNode, null));
+                        if (checkoutNode) {
+                            logger.debug("Checking in node");
+                            checkOutCheckInService.checkin(writeNode, null);
+                        }
                     } catch (Exception ex) {
                         if (!writer.isClosed()) {
                             try {
@@ -120,12 +142,12 @@ public class ConvertAction extends ActionExecuterAbstractBase {
                             }
                         }
 
-                        if (nodeService.exists(node)) {
+                        if (deleteNode && nodeService.exists(writeNode)) {
                             logger.debug("Deleting created node");
-                            nodeService.deleteNode(node);
+                            nodeService.deleteNode(writeNode);
                         }
 
-                        throw new WebScriptException("Conversion failed", ex);
+                        throw new AlfrescoRuntimeException("Conversion failed", ex);
                     } finally {
                         if (!reader.isClosed()) {
                             try {
@@ -134,12 +156,17 @@ public class ConvertAction extends ActionExecuterAbstractBase {
                                 logger.error("Error close stream", e);
                             }
                         }
+
+                        if (nodeService.exists(writeNode) && checkOutCheckInService.isCheckedOut(writeNode)) {
+                            logger.debug("Finally: cancelCheckout()");
+                            checkOutCheckInService.cancelCheckout(writeNode);
+                        }
                     }
                 } else {
-                    throw new SecurityException("User have no permission to create node");
+                    throw new AlfrescoRuntimeException("The node is already locked");
                 }
             } else {
-                throw new SecurityException("User have no read access");
+                throw new SecurityException("User don't have the permissions to read this node");
             }
         }
     }
