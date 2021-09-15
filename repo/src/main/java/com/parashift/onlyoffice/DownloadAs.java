1dede7e2e0d5af4b021a1d69e63132dcb7a9ad0a
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2021-09-15 10:02:57 +0300

    refactoring download as handler

--- repo/src/main/java/com/parashift/onlyoffice/DownloadAs.java
+++ repo/src/main/java/com/parashift/onlyoffice/DownloadAs.java
@@ -1,32 +1,33 @@
 package com.parashift.onlyoffice;
 
 import org.alfresco.model.ContentModel;
+import org.alfresco.repo.download.DownloadModel;
+import org.alfresco.repo.download.DownloadStatusUpdateService;
 import org.alfresco.repo.download.DownloadStorage;
 import org.alfresco.repo.security.permissions.AccessDeniedException;
-import org.alfresco.service.cmr.download.DownloadService;
+import org.alfresco.service.cmr.download.DownloadStatus;
 import org.alfresco.service.cmr.repository.*;
 import org.alfresco.service.cmr.security.AccessStatus;
 import org.alfresco.service.cmr.security.PermissionService;
-import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
 import org.alfresco.util.TempFileProvider;
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
-import org.json.simple.JSONArray;
-import org.json.JSONException;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.extensions.surf.util.URLEncoder;
 import org.springframework.extensions.webscripts.*;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.MessageFormat;
 import java.util.*;
-import java.util.zip.Adler32;
-import java.util.zip.CheckedOutputStream;
 import java.util.zip.Deflater;
 
 /*
@@ -39,6 +40,9 @@ public class DownloadAs extends AbstractWebScript {
     private static final String MIMETYPE_ZIP = "application/zip";
     private static final String TEMP_FILE_PREFIX = "alf";
     private static final String ZIP_EXTENSION = ".zip";
+    private final static String CONTENT_DOWNLOAD_API_URL = "slingshot/node/content/{0}/{1}/{2}/{3}";
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     Converter converterService;
@@ -56,135 +60,140 @@ public class DownloadAs extends AbstractWebScript {
     ContentService contentService;
 
     @Autowired
+    DownloadStorage downloadStorage;
+
+    @Autowired
     MimetypeService mimetypeService;
 
     @Autowired
-    DownloadService downloadService;
-
-    @Autowired
-    DownloadStorage downloadStorage;
+    DownloadStatusUpdateService updateService;
 
     @Override
     public void execute(WebScriptRequest request, WebScriptResponse response) throws IOException {
-        NodeRef downloadsContainerNodeRef = downloadStorage.getOrCreateDowloadContainer();
-        response.setContentType("application/json; charset=utf-8");
-        response.setContentEncoding("UTF-8");
-        JSONObject responseJson = new JSONObject();
-        if (request.getContent().getContent().length() != 2) {
-            try {
-                JSONParser parser = new JSONParser();
-                org.json.simple.JSONObject json = (org.json.simple.JSONObject) parser.parse(request.getContent().getContent());
-                responseJson.put("downloadUrl", createZipWithMultipleFiles(downloadsContainerNodeRef, (JSONArray) json.get("list")));
-                response.getWriter().write(responseJson.toString(3));
-            } catch (JSONException | ParseException e) {
-                e.printStackTrace();
-                throw new WebScriptException("Json parse exception: " + e.getMessage());
-            }
-        } else if (request.getParameter("nodeRef") != null) {
-            NodeRef nodeRef = new NodeRef(request.getParameter("nodeRef"));
-            if (request.getParameter("outputType") != null) {
-                if (permissionService.hasPermission(nodeRef, PermissionService.READ) != AccessStatus.ALLOWED) {
-                    throw new AccessDeniedException("Access denied. You do not have the appropriate permissions to perform this operation");
+        try {
+            String contentURL = null;
+            JSONArray requestDataJson = new JSONArray(request.getContent().getContent());
+
+            if (requestDataJson.length() == 1) {
+                JSONObject data = requestDataJson.getJSONObject(0);
+
+                NodeRef node = new NodeRef(data.getString("nodeRef"));
+                String outputType = data.getString("outputType");
+
+                if (permissionService.hasPermission(node, PermissionService.READ) != AccessStatus.ALLOWED) {
+                    throw new AccessDeniedException("Access denied. You do not have the appropriate permissions to perform this operation. NodeRef= " + node.toString());
                 }
-                String outputType = request.getParameter("outputType");
-                String srcType = request.getParameter("srcType");
-                try {
-                    NodeRef convertedNode = convertNode(nodeRef, outputType, srcType, downloadsContainerNodeRef);
-                    if (convertedNode != null) {
-                        responseJson.put("downloadUrl", util.getContentUrl(convertedNode));
-                    } else {
-                        throw new WebScriptException("ConvertNodeRef is null.");
+
+                Map<QName, Serializable> properties = nodeService.getProperties(node);
+                String docTitle = (String) properties.get(ContentModel.PROP_NAME);
+                String currentExt = docTitle.substring(docTitle.lastIndexOf(".") + 1).trim().toLowerCase();
+
+                if (currentExt.equals(outputType)) {
+                    contentURL = getDownloadAPIUrl(node, docTitle);
+                } else {
+                    String downloadUrl = converterService.convert(util.getKey(node), currentExt, outputType, util.getContentUrl(node));
+                    docTitle = docTitle.substring(0, docTitle.lastIndexOf(".") + 1) + outputType;
+                    URL url = new URL(util.replaceDocEditorURLToInternal(downloadUrl));
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    try (InputStream inputStream = connection.getInputStream()) {
+                        contentURL = createDownloadNode(docTitle, mimetypeService.getMimetype(outputType), inputStream, connection.getContentLength(), 1);
                     }
-                    response.getWriter().write(responseJson.toString(3));
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    throw new WebScriptException("Unable to convert: " + e.getMessage());
                 }
             } else {
-                throw new WebScriptException("Output type is null");
-            }
-        } else {
-            throw new WebScriptException(Status.STATUS_BAD_REQUEST, "Could not find required 'nodeRef' parameter or request body");
-        }
-    }
+                File zip = TempFileProvider.createTempFile(TEMP_FILE_PREFIX, ZIP_EXTENSION);
+                long totalSize = 0;
 
-    private String createZipWithMultipleFiles(NodeRef downloadsContainerNodeRef, JSONArray nodeRefToConvert) {
-        Map<QName, Serializable> props = new HashMap<>(1);
-        props.put(ContentModel.PROP_NAME, "content.zip");
-        NodeRef zipContentNodeRef = nodeService.createNode(downloadsContainerNodeRef, ContentModel.ASSOC_CHILDREN,
-                QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, "content.zip"),
-                ContentModel.TYPE_CONTENT, props).getChildRef();
+                try (FileOutputStream stream = new FileOutputStream(zip);
+                     ZipArchiveOutputStream out = new ZipArchiveOutputStream(stream)) {
 
-        try {
-            File zip = TempFileProvider.createTempFile(TEMP_FILE_PREFIX, ZIP_EXTENSION);
-            FileOutputStream stream = new FileOutputStream(zip);
-            CheckedOutputStream checksum = new CheckedOutputStream(stream, new Adler32());
-            BufferedOutputStream buff = new BufferedOutputStream(checksum);
-            ZipArchiveOutputStream out = new ZipArchiveOutputStream(buff);
-            out.setEncoding("UTF-8");
-            out.setMethod(ZipArchiveOutputStream.DEFLATED);
-            out.setLevel(Deflater.BEST_COMPRESSION);
+                    out.setEncoding("UTF-8");
+                    out.setMethod(ZipArchiveOutputStream.DEFLATED);
+                    out.setLevel(Deflater.BEST_COMPRESSION);
 
-            for (int i=0; i < nodeRefToConvert.size(); i++) {
-                org.json.simple.JSONObject object = (org.json.simple.JSONObject) nodeRefToConvert.get(i);
-                NodeRef nodeToConvert = new NodeRef(object.get("nodeRef").toString());
-                if (permissionService.hasPermission(nodeToConvert, PermissionService.READ) != AccessStatus.ALLOWED) {
-                    throw new AccessDeniedException("Access denied. You do not have the appropriate permissions to perform this operation. NodeRef= " + nodeToConvert.toString());
-                }
-                String srcType = object.get("srcType").toString();
-                String outputType = object.get("outputType").toString();
-                NodeRef convertedNode = nodeToConvert;
-                if (!srcType.equals(outputType)) {
-                    convertedNode = convertNode(nodeToConvert, outputType,
-                            srcType, downloadsContainerNodeRef);
-                }
-                ContentReader reader = contentService.getReader(convertedNode, ContentModel.PROP_CONTENT);
-                if (reader != null) {
-                    InputStream is = reader.getContentInputStream();
-                    ZipArchiveEntry zipEntry = new ZipArchiveEntry(nodeService.getProperty(convertedNode, ContentModel.PROP_NAME).toString());
-                    out.putArchiveEntry(zipEntry);
-                    byte[] bytes = new byte[1024];
-                    int length;
-                    while ((length = is.read(bytes)) >= 0) {
-                        out.write(bytes, 0, length);
+                    for (int i = 0; i < requestDataJson.length(); i++) {
+                        JSONObject data = requestDataJson.getJSONObject(i);
+
+                        NodeRef node = new NodeRef(data.getString("nodeRef"));
+                        String outputType = data.getString("outputType");
+
+                        if (permissionService.hasPermission(node, PermissionService.READ) != AccessStatus.ALLOWED) {
+                            throw new AccessDeniedException("Access denied. You do not have the appropriate permissions to perform this operation. NodeRef= " + node.toString());
+                        }
+
+                        Map<QName, Serializable> properties = nodeService.getProperties(node);
+                        String docTitle = (String) properties.get(ContentModel.PROP_NAME);
+                        String currentExt = docTitle.substring(docTitle.lastIndexOf(".") + 1).trim().toLowerCase();
+
+                        if (currentExt.equals(outputType)) {
+                            ContentReader reader = contentService.getReader(node, ContentModel.PROP_CONTENT);
+                            try (InputStream inputStream = reader.getContentInputStream()) {
+                                out.putArchiveEntry(new ZipArchiveEntry(docTitle));
+                                totalSize = totalSize + IOUtils.copyLarge(inputStream, out);
+                            }
+                        } else {
+                            String downloadUrl = converterService.convert(util.getKey(node), currentExt, outputType, util.getContentUrl(node));
+                            docTitle = docTitle.substring(0, docTitle.lastIndexOf(".") + 1) + outputType;
+                            URL url = new URL(util.replaceDocEditorURLToInternal(downloadUrl));
+                            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                            try (InputStream inputStream = connection.getInputStream()) {
+                                out.putArchiveEntry(new ZipArchiveEntry(docTitle));
+                                totalSize = totalSize + IOUtils.copyLarge(inputStream, out);
+                            }
+                        }
+
+                        out.closeArchiveEntry();
                     }
-                    out.closeArchiveEntry();
-                    is.close();
+                }
+
+                try (FileInputStream inputStream = new FileInputStream(zip)) {
+                    contentURL = createDownloadNode("Archive.zip", MIMETYPE_ZIP, inputStream, totalSize, requestDataJson.length());
                 }
             }
-            out.close();
-            buff.close();
-            checksum.close();
-            stream.close();
-            ContentWriter writer = contentService.getWriter(zipContentNodeRef, ContentModel.PROP_CONTENT, true);
-            writer.setMimetype(MIMETYPE_ZIP);
-            writer.putContent(zip);
-            return util.getContentUrl(zipContentNodeRef);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new WebScriptException("Error " + e.getMessage());
+
+            JSONObject responseJson = new JSONObject();
+            responseJson.put("downloadUrl", contentURL);
+
+            response.setContentType("application/json; charset=utf-8");
+            response.setContentEncoding("UTF-8");
+            response.getWriter().write(responseJson.toString(3));
+
+        } catch(Exception e){
+            throw new WebScriptException(e.getMessage(), e);
         }
     }
-    private NodeRef convertNode(NodeRef nodeToConvert, String outputType, String srcType, NodeRef downloadsContainerNodeRef) {
-        try {
-            InputStream in;
-            String nodeName = nodeService.getProperty(nodeToConvert, ContentModel.PROP_NAME).toString();
-            String convertedNodeName = nodeName.substring(0, nodeName.lastIndexOf(".") + 1) + outputType;
-            Map<QName, Serializable> props = new HashMap<>(1);
-            props.put(ContentModel.PROP_NAME, convertedNodeName);
-            String downloadUrl = converterService.convert(util.getKey(nodeToConvert), srcType, outputType, util.getContentUrl(nodeToConvert));
-            NodeRef convertedNode = nodeService.createNode(downloadsContainerNodeRef, ContentModel.ASSOC_CHILDREN,
-                    QName.createQName(NamespaceService.CONTENT_MODEL_1_0_URI, convertedNodeName), ContentModel.TYPE_CONTENT, props).getChildRef();
-            URL url = new URL(downloadUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            in = connection.getInputStream();
-            ContentWriter writer = contentService.getWriter(convertedNode, ContentModel.PROP_CONTENT, true);
-            writer.setMimetype(mimetypeService.getMimetype(outputType));
-            writer.putContent(in);
-            return convertedNode;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
+
+    private String createDownloadNode (String title, String mimeType, InputStream inputStream, long totalSize, long totalFiles) {
+        NodeRef downloadNode = nodeService.createNode(
+                downloadStorage.getOrCreateDowloadContainer(),
+                ContentModel.ASSOC_CHILDREN,
+                ContentModel.ASSOC_CHILDREN,
+                DownloadModel.TYPE_DOWNLOAD,
+                Collections.<QName, Serializable> singletonMap(ContentModel.PROP_NAME, title)).getChildRef();
+
+        Map<QName, Serializable> aspectProperties = new HashMap<>(2);
+        aspectProperties.put(ContentModel.PROP_IS_INDEXED, Boolean.FALSE);
+        aspectProperties.put(ContentModel.PROP_IS_CONTENT_INDEXED, Boolean.FALSE);
+        nodeService.addAspect(downloadNode, ContentModel.ASPECT_INDEX_CONTROL, aspectProperties);
+
+        ContentWriter writer = contentService.getWriter(downloadNode, ContentModel.PROP_CONTENT, true);
+        writer.setMimetype(mimeType);
+        writer.putContent(inputStream);
+
+        DownloadStatus status = new DownloadStatus(DownloadStatus.Status.DONE, totalSize, totalSize, totalFiles, totalFiles);
+        int sequenceNumber = downloadStorage.getSequenceNumber(downloadNode) + 1;
+        updateService.update(downloadNode, status, sequenceNumber);
+
+        return getDownloadAPIUrl(downloadNode, title);
+    }
+
+    private String getDownloadAPIUrl (NodeRef nodeRef, String title) {
+        String contentURL = MessageFormat.format(
+                CONTENT_DOWNLOAD_API_URL, new Object[]{
+                        nodeRef.getStoreRef().getProtocol(),
+                        nodeRef.getStoreRef().getIdentifier(),
+                        nodeRef.getId(),
+                        URLEncoder.encode(title)});
+
+        return contentURL;
     }
 }
