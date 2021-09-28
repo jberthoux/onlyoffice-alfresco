b52d05c883a67e8801492cbda291878fa727ee8a
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2021-09-28 10:55:10 +0300

    request in onlyoffice-settings only when opening a window

--- share/src/main/resources/META-INF/components/documentlibrary/onlyoffice-doclib-multi-download-as.js
+++ share/src/main/resources/META-INF/components/documentlibrary/onlyoffice-doclib-multi-download-as.js
@@ -1,16 +1,6 @@
 (function () {
     var supportedFormats = null;
 
-    Alfresco.util.Ajax.jsonGet({
-        url: Alfresco.constants.PROXY_URI + "parashift/onlyoffice/onlyoffice-settings",
-        successCallback: {
-            fn: function(response) {
-                supportedFormats = response.json.supportedFormats;
-            },
-            scope: this
-        },
-    });
-
     var getOutputTypes = function (docExt) {
         var outputTypes = null;
 
@@ -70,7 +60,7 @@
                 scope: this
             },
             failureCallback: {
-                fn: function exampleFailure() {
+                fn: function () {
                     waitDialog.destroy()
                 },
                 scope: this
@@ -78,103 +68,136 @@
         });
     };
 
+    var dialogOnlyofficeDownloadAs = function (record) {
+        var nodeList = [];
+        if (Array.isArray(record)) {
+            for (var node of record) {
+                nodeList.push(node);
+            }
+        } else {
+            nodeList.push(record);
+        }
+
+        var body = [];
+        body.push(
+            "<label>" + Alfresco.util.message("alfresco.document-onlyoffice-download-as.form.field.select-type") + "</label>",
+            "<div class='download-as-main'>"
+        );
+
+        var showCheckbox = nodeList.length == 1 ? "hidden" : "";
+        for (var node of nodeList) {
+            var ext = node.displayName.substring(node.displayName.lastIndexOf(".") + 1);
+            var outputTypes = getOutputTypes(ext);
+
+            if (node.jsNode.mimetype != undefined && outputTypes != null) {
+                body.push(
+                    "<div class='download-as-file'>",
+                    "<input type='checkbox' class='download-as-checkbox " + showCheckbox + "' checked='true' value='" + node.nodeRef + "'>",
+                    "<p class='download-as-filename'>" + node.displayName + "</p>",
+                    "<p class='download-as-convert-label'>" + Alfresco.util.message("alfresco.document-onlyoffice-download-as.form.field.p.convert-into") + "</p>",
+                    "<select class='download-as-select'>"
+                );
+
+                for (var type of outputTypes) {
+                    body.push("<option>" + type + "</option>");
+                }
+
+                body.push(
+                    "</select>",
+                    "</div>"
+                );
+            }
+        }
+
+        body.push("</div>");
+
+        var prompt = new YAHOO.widget.SimpleDialog("prompt", {
+            close:true,
+            constraintoviewport: true,
+            draggable: false,
+            effect: null,
+            modal: true,
+            visible: false,
+            zIndex: this.zIndex++,
+            buttons: [
+                {
+                    text: Alfresco.util.message("alfresco.document-onlyoffice-download-as.form.submit-button"),
+                    handler: function onAction_success() {
+                        var requestData = [];
+                        var fileList = document.getElementsByClassName("download-as-file");
+
+                        for (var file of fileList) {
+                            var checkbox = file.querySelector(".download-as-checkbox");
+                            if (checkbox.checked) {
+                                var select = file.querySelector(".download-as-select");
+                                requestData.push({
+                                    nodeRef: checkbox.value,
+                                    outputType: select.value
+                                });
+                            }
+                        }
+
+                        if (requestData.length > 0) {
+                            this.destroy();
+                            multipleDownloadPost(requestData);
+                        }
+                    }
+                },
+                {
+                    text : Alfresco.util.message("button.cancel"),
+                    handler : function onAction_cancel() {
+                        this.destroy();
+                    },
+                    isDefault : true
+                }
+            ]
+        });
+
+        prompt.setHeader(Alfresco.util.message("actions.document.onlyoffice-download-as"));
+        prompt.setBody(body.join(""));
+        prompt.render(document.body);
+        prompt.center();
+        prompt.show();
+
+        YAHOO.util.Event.removeListener(prompt.close, "click");
+        YAHOO.util.Event.on(prompt.close, "click", function() {
+            this.destroy();
+        }, prompt, true);
+    }
+
     YAHOO.Bubbling.fire("registerAction", {
         actionName: "onOnlyofficeDownloadAs",
         fn: function (record, owner) {
-            var nodeList = [];
-            if (Array.isArray(record)) {
-                for (var node of record) {
-                    nodeList.push(node);
-                }
-            } else {
-                nodeList.push(record);
-            }
+            if (!supportedFormats) {
+                var waitDialog = Alfresco.util.PopupManager.displayMessage({
+                    text : "",
+                    spanClass : "wait",
+                    displayTime : 0
+                });
 
-            var body = [];
-            body.push(
-                "<label>" + this.msg("alfresco.document-onlyoffice-download-as.form.field.select-type") + "</label>",
-                "<div class='download-as-main'>"
-            );
-
-            var showCheckbox = nodeList.length == 1 ? "hidden" : "";
-            for (var node of nodeList) {
-                var ext = node.displayName.substring(node.displayName.lastIndexOf(".") + 1);
-                var outputTypes = getOutputTypes(ext);
-
-                if (node.jsNode.mimetype != undefined && outputTypes != null) {
-                    body.push(
-                        "<div class='download-as-file'>",
-                        "<input type='checkbox' class='download-as-checkbox " + showCheckbox + "' checked='true' value='" + node.nodeRef + "'>",
-                        "<p class='download-as-filename'>" + node.displayName + "</p>",
-                        "<p class='download-as-convert-label'>" + this.msg("alfresco.document-onlyoffice-download-as.form.field.p.convert-into") + "</p>",
-                        "<select class='download-as-select'>"
-                    );
-
-                    for (var type of outputTypes) {
-                        body.push("<option>" + type + "</option>");
-                    }
-
-                    body.push(
-                        "</select>",
-                        "</div>"
-                    );
-                }
-            }
-
-            body.push("</div>");
-
-            var prompt = new YAHOO.widget.SimpleDialog("prompt", {
-                close:true,
-                constraintoviewport: true,
-                draggable: false,
-                effect: null,
-                modal: true,
-                visible: false,
-                zIndex: this.zIndex++,
-                buttons: [
-                    {
-                        text: Alfresco.util.message("alfresco.document-onlyoffice-download-as.form.submit-button"),
-                        handler: function onAction_success() {
-                            var requestData = [];
-                            var fileList = document.getElementsByClassName("download-as-file");
-
-                            for (var file of fileList) {
-                                var checkbox = file.querySelector(".download-as-checkbox");
-                                if (checkbox.checked) {
-                                    var select = file.querySelector(".download-as-select");
-                                    requestData.push({
-                                        nodeRef: checkbox.value,
-                                        outputType: select.value
-                                    });
-                                }
-                            }
-
-                            if (requestData.length > 0) {
-                                this.destroy();
-                                multipleDownloadPost(requestData);
-                            }
-                        }
-                    },
-                    {
-                        text : Alfresco.util.message("button.cancel"),
-                        handler : function onAction_cancel() {
-                            this.destroy();
+                Alfresco.util.Ajax.jsonGet({
+                    url: Alfresco.constants.PROXY_URI + "parashift/onlyoffice/onlyoffice-settings",
+                    successCallback: {
+                        fn: function(response) {
+                            waitDialog.destroy()
+                            supportedFormats = response.json.supportedFormats;
+                            dialogOnlyofficeDownloadAs(record);
                         },
-                        isDefault : true
+                        scope: this
+                    },
+                    failureCallback: {
+                        fn: function () {
+                            waitDialog.destroy()
+                            Alfresco.util.PopupManager.displayMessage({
+                                text: Alfresco.util.message("alfresco.document.onlyoffice.action.download-as.msg.failure-multi")
+                            });
+                        },
+                        scope: this
                     }
-                ]
-            });
-
-            prompt.setHeader(Alfresco.util.message("actions.document.onlyoffice-download-as"));
-            prompt.setBody(body.join(""));
-            prompt.render(document.body);
-            prompt.center();
-            prompt.show();
-
-            YAHOO.util.Event.removeListener(prompt.close, "click");
-            YAHOO.util.Event.on(prompt.close, "click", function() {
-                this.destroy();
-            }, prompt, true);
+                });
+            } else {
+                dialogOnlyofficeDownloadAs(record);
+            }
         }
     });
 })();
